8b67e41aa3d6afaf0775feddead6ca8ebcaba855
shub2710 <shubham.kumar008@gmail.com>
2019-09-02 18:41:09 +0530

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -16,34 +16,9 @@ class MyDocument extends Document {
                crossorigin="anonymous"
         />
         <link  rel="stylesheet" href="static/css/carousel.min.css" />
+        <link  rel="stylesheet" href="static/css/global.css" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.6/holder.js" />
-        <style global jsx>{`
-           
-      .content {
-        max-width: 350px;
-        margin: auto;
-        background: white;
-        padding: 10px;
-      }
-      .finish {
-          padding : 20px;
-      }
-      .section1 {
-        white-space: nowrap;
-      }
-      .slides{
-        display: inline-block;
-      }
-      .selectSlide  {
-            color : #007bff;
-            display: inline-block;
-        }
-      .numberToText{
-        margin-bottom: 31px;
-         color:#fff;
-         padding : 7px;
-      }
-      `}</style>
+  
         </Head>
         <body>
           <Main />
